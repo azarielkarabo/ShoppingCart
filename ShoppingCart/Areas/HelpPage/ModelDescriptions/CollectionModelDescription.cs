0fc5f6e3a8a0a2c9273eb542bd8ae6a740d28b88
azarielm@boxfusion.co.za <azarielm@boxfusion.co.za>
2020-04-18 22:04:21 +0200

    Removed heplers folder

--- ShoppingCart/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ShoppingCart.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- ShoppingCart/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ShoppingCart.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
